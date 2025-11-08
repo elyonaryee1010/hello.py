460a1c964ca7995f421fdddb714a17fbc3bd20e8
Elyon <duboisaryee@gmail.com>
2025-11-08 16:42:28 +0000

    Refactor pythoncode.py with arithmetic and string ops
    Removed comments and added various arithmetic operations along with string manipulations.

--- pythoncode.py
+++ pythoncode.py
@@ -9,15 +9,6 @@ print("hello")
 print("Sum of 3 and 4 is : ",3+4)
 print("Product of 3 and 4 is : ",3*4)
 
-#This is a python file
-
-'''
-This is a multi line comments
-
-'''
-
-
-
 #New code penguin
 name = "Penguin"
 age = 15
@@ -39,3 +30,42 @@ print("Data Type of age is", type(age))
 weight = int(weight)
 print(weight)
 print("Data Type of Weight is", type(weight))
+
+num1 = 45
+num2 = 3
+
+print("Number 1", num1)
+print("Number 2", num2)
+print("Addition: ", num1+num2)
+print("Product: ", num1*num2)
+print("Division :", num1/num2)
+print("Floor Division:", num1//num2)
+print("Modulus Operation :", num1%num2)
+print("Square :", num2**2)
+print("Square Root :", num1**0.5)
+print("Equal ?", num1==num2)
+print("Number 1 greater?", num1>num2)
+print("Number 2 greater?", num1<num2
+print("Not equal ?", num1!=num2)
+
+
+result = num1/2+num2**2+10
+print("RESULT OF GIVEN EQUATION IS:", result)
+
+
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name+last_name
+example = "Haa"*3
+
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("String Multiplied 5 times gives this result :", example)
+
+
+word = 'Coding'
+print("Length of String :", len(word))
+print("First Letter of String :", word[0])
+print("Last Letter of String :", word[5])
+print("String Sliced :", word[0:3])
