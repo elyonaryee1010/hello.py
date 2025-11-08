4273e91a6a1193fbfb07b01a6e9f60f438460af8
Elyon <duboisaryee@gmail.com>
2025-11-08 16:19:12 +0000

    Add variable declarations and type checks
    Added new variables and type checks for name, age, and is_student. Included weight variable usage and type casting examples.

--- pythoncode.py
+++ pythoncode.py
@@ -8,3 +8,34 @@ This is a multi line comments
 print("hello")
 print("Sum of 3 and 4 is : ",3+4)
 print("Product of 3 and 4 is : ",3*4)
+
+#This is a python file
+
+'''
+This is a multi line comments
+
+'''
+
+
+
+#New code penguin
+name = "Penguin"
+age = 15
+is_student = True
+
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print("is_student :", is_student)
+print("Data type of is_student is", type(is_student))
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+
+print("/n After Type Casting...")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
