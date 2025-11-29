9f8335f73602653a71a264418f50f54b160aa2d7
Elyon <duboisaryee@gmail.com>
2025-11-29 16:00:05 +0000

    Implement turtle graphics and data structure examples
    Added turtle graphics to draw shapes including a polygon, triangle, and rectangle. Included examples of tuples and sets.

--- pythoncode.py
+++ pythoncode.py
@@ -310,6 +310,140 @@ print("\nOriginal list of list:", student_list)
 print("Convert the said list of lists to a dictionary:", student_list)
 
 
+import turtle
+turtle.Screen().bgcolor("red")
+turtle.Screen().setup(500,500)
+polygon = turtle.Turtle()
+num_sides = 6
+side_length = 70
+angle= 360/(num_sides)
+for i in range(num_sides):
+    polygon.forward(side_length)
+    polygon.right(angle)
+turtle.done()
+
+#Triangle
+turtle.Screen().bgcolor("Aqua")
+board = turtle.Turtle()
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(90)
+board.forward(100)
+
+board.right(12)
+board.forward(100)
+board.right(120)
+board.forward(100)
+
+turtle.done()
+
+
+#Rectangle
+
+turtle.Screen().bgcolor("Aquamarine")
+board = turtle.Turtle()
+
+board.forward(100)
+board.right(90)
+board.forward(100)
+board.right(90)
+
+board.forward(150)
+
+board.forward(100)
+board.right(90)
+board.forward(100)
+board.right(90)
+board.forward(100)
+board.right(90)
+board.right(90)
+board.right(90)
+board.right(90)
+board.forward(100)
+
+
+board.penup()
+board.right(100)
+
+board.forward(90)
+board.right(100)
+
+board.penup()
+board.right(100)
+
+
+
+
+
+
+turtle.done()
+
+#Python Data Assingment.
+#secret_number = 10
+##results ={}
+
+#while True:
+    #guess = int(input("Try and find the number the judge has picked"))
+    #if guess == secret_number:
+        #print("You got it.")
+    #else:
+        #print("WRONG")
+
+#results['final_number'] = secret_number
+
+#print(f"You got it! The was {results['final_number']}")
+
+#Data Structures in Python -2
+my_tuple = ()
+print(my_tuple)
+my_tuple =(1,2,3)
+print(my_tuple)
+
+my_tuple =(1,2,3, "hello")
+print("my_tuple")
+
+my_tuple =(1,2,3, "hello", 4.5)
+print(my_tuple)
+
+my_tuple =(1,2,3, "hello")
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[2])
+print(my_tuple[5])
+print(my_tuple[10])
+
+print(my_tuple[0:3])
+
+##Activity-2
+my_set={1,2,2,3,4,4,5,6,6,7,7,8,8,8}
+print("my_set", my_set)
+
+my_set.add(10)
+print("updated", my_set)
+
+set1=my_set
+set2=(10,9,2,56,79)
+print("difference is:", set1.difference(set2))
+
+#activity-3
+setun1={"purple", "red"}
+setun2={"white", "orange"}
+print("union", setun1.union(setun2))
+
+## activity-4
+setx={"plum", "apple"}
+sety={"apple", "grape"}
+print("intersection", setx.intersection(sety))
+
+              
+
+
 
 
 
